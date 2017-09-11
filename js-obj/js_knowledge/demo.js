00fdc78759f2f9e1a30ba6cfa7400b0669fff0e7
sssudu <1150221563@qq.com>
2017-09-11 15:28:10 +0800

    0911

--- js-obj/js_knowledge/demo.js
+++ js-obj/js_knowledge/demo.js
@@ -1,5 +1,25 @@
-for (var i = 0; i < 5; i++) {
-    setTimeout(function() {
-        console.log(i);
-    }, 0)
+// for (var i = 0; i < 5; i++) {
+//     //闭包传参
+//     //每次创建传当前数值进去
+//     (function(n) {
+//         setTimeout(function() {
+//             console.log(n);
+//         }, 0)
+//     })(i);
+// }
+
+
+function fun(n, o) {
+    console.log(o);
+    return {
+        method: function(m) {
+            return fun(m, n);
+        }
+    }
 }
+
+var a = fun(0);
+a.method(1);
+a.method(2);
+
+fun(0).method(1).method(2).method(3);
